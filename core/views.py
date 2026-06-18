c9ee57ace9a74a2511ca07347875e9d487b99608
Rurizim <ruandjr48@gmail.com>
2026-06-18 16:42:20 -0300

    adicionando informações

--- core/views.py
+++ core/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
     # render == ferramenta que renderiza o html
 
 def ola_mundo(request):
-    return render(request, 'index.html')
+    contexto = {
+        'disciplina': 'fundamentos de redes de computadores',
+        'total_de_aulas': 0,
+        'aulas_concluidas': 0
+    }
+    return render(request, 'index.html', contexto)
